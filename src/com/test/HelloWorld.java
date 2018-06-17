ef68a602069249ea628d5bd3c867ba8641a7f8a9
caohuikang <2205859512@qq.com>
2018-06-17 10:58:23 +0800

    commit hello test

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author huikang.chk
+ * @date 2018/6/17
+ */
+public class HelloWorld {
+    public  static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
